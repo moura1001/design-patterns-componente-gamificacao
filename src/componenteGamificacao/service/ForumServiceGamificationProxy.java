08aad7b8b29b077768f38f96c06b555307efd1fc
moura1001 <gmouraneto01@gmail.com>
2024-03-15 19:02:24 -0300

    feat: utilização do padrão de projeto Proxy para invocação das funcionalidades de gamification de forma transparente à aplicação

---
+++ src/componenteGamificacao/service/ForumServiceGamificationProxy.java
@@ -0,0 +1,21 @@
+package componenteGamificacao.service;
+
+public class ForumServiceGamificationProxy implements ForumService {
+
+	@Override
+	public void addTopic(String user, String topic) {
+	}
+
+	@Override
+	public void addComment(String user, String topic, String comment) {
+	}
+
+	@Override
+	public void likeTopic(String user, String topic, String topicUser) {
+	}
+
+	@Override
+	public void likeComment(String user, String topic, String comment, String commentUser) {
+	}
+
+}
